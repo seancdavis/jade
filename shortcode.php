ef7d7505ee99da3524e4e265acd08d16ef12c12a
Sean C. Davis <scdavis41@gmail.com>
2013-03-27 07:36:01 -0400

    Added featured=true as an option to display featured product only. Widget option and styling are only two pieces left.

--- shortcode.php
+++ shortcode.php
@@ -1,22 +1,38 @@
 <?php
-//global $jade;
 
 extract( shortcode_atts( array(
-	'' => ''
+	'featured' => false
 ), $atts ) );
 
-global $post;
-$loop = new WP_Query( array ( 'post_type' => 'rt_jade', 'posts_per_page' => '100' ) );
-while ( $loop->have_posts() ) : $loop->the_post(); ?>
+if( $featured == false ) {
+	global $post;
+	$loop = new WP_Query( array ( 'post_type' => 'rt_jade', 'posts_per_page' => '100' ) );
+	while ( $loop->have_posts() ) : $loop->the_post(); ?>
+		
+		<h2 class="rt-product-title">
+			<span id="rt-product-<?php echo $post->ID; ?>" class="rt-product-icon">+</span>
+			<?php the_title(); ?>
+		</h2>
+		<div id="rt-product-description-<?php echo $post->ID; ?>" class="rt-product-description clearfix">
+			<span class="rt-product-price">$<?php echo get_post_meta($post->ID, '_price', true); ?></span>
+			<a class="rt-product-preview" href="<?php echo get_post_meta($post->ID, '_sample_profile', true); ?>" target="_blank">Sample Profile</a>
+			<?php the_content(); ?>
+		</div>
+		
+	<?php endwhile;
+}
+else {
+	global $jade; 
+	$id = $jade->option('featured_product'); ?>
+	<h2 class=""><?php echo get_the_title($id); ?></h2>
+	<p>$<?php echo get_post_meta($id, '_price', true); ?></p>
+	<a class="" href="<?php echo get_post_meta($id, '_sample_profile', true); ?>" target="_blank">Sample Profile</a>
+	<p>
+		<?php 
+		$feat_post = get_post( $id );
+		echo $feat_post->post_content; ?>
+	</p>
 	
-	<h2 class="rt-product-title">
-		<span id="rt-product-<?php echo $post->ID; ?>" class="rt-product-icon">+</span>
-		<?php the_title(); ?>
-	</h2>
-	<div id="rt-product-description-<?php echo $post->ID; ?>" class="rt-product-description clearfix">
-		<span class="rt-product-price">$<?php echo get_post_meta($post->ID, '_price', true); ?></span>
-		<a class="rt-product-preview" href="<?php echo get_post_meta($post->ID, '_sample_profile', true); ?>" target="_blank">Sample Profile</a>
-		<?php the_content(); ?>
-	</div>
-	
-<?php endwhile; ?>
+<?php } 
+
+?>
