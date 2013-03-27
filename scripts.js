8c99f24b9b8c82725e2096458f7eb1ca2b7488a5
Sean C. Davis <scdavis41@gmail.com>
2013-03-27 15:18:53 -0400

    Styled featured product. Made product list into accordion menu. All looking good and working. About ready to consider this one to be at v0.1 after a little more checking.

--- scripts.js
+++ scripts.js
@@ -1,5 +1,22 @@
 jQuery(document).ready(function($) {
 	
+	$('.rt-product-icon').click(function(){
+		
+		var id = $(this).attr('id').substr(11);
+		
+		if( $(this).html() == '–' ) {	
+			$(this).html('+');
+			$('#rt-product-description-'+id).slideUp();
+		}
+		else {
+			$('.rt-product-description').slideUp();
+			$('.rt-product-icon').html('+');
+			$(this).html('–');
+			$('#rt-product-description-'+id).slideDown();
+		}
+	});
+	
+	/*
 	$('.rt-product-icon').toggle(function(){
 		var id = $(this).attr('id').substr(11);
 		$(this).html('&#8722;');
@@ -8,6 +25,6 @@ jQuery(document).ready(function($) {
 		var id = $(this).attr('id').substr(11);
 		$(this).html('+');
 		$('#rt-product-description-'+id).slideUp();
-	});
+	});*/
 	
 });
--- shortcode.php
+++ shortcode.php
@@ -4,34 +4,38 @@ extract( shortcode_atts( array(
 	'featured' => false
 ), $atts ) );
 
-if( $featured == false ) {
-	global $post;
+if( $featured == false ) { ?>
+	<hr class="rt-product-line">
+	<?php global $post;
 	$loop = new WP_Query( array ( 'post_type' => 'rt_jade', 'posts_per_page' => '100' ) );
 	while ( $loop->have_posts() ) : $loop->the_post(); ?>
-		
 		<h2 class="rt-product-title">
 			<span id="rt-product-<?php echo $post->ID; ?>" class="rt-product-icon">+</span>
 			<?php the_title(); ?>
 		</h2>
 		<div id="rt-product-description-<?php echo $post->ID; ?>" class="rt-product-description clearfix">
-			<span class="rt-product-price">$<?php echo get_post_meta($post->ID, '_price', true); ?></span>
 			<a class="rt-product-preview" href="<?php echo get_post_meta($post->ID, '_sample_profile', true); ?>" target="_blank">Sample Profile</a>
+			<span class="rt-product-price">$<?php echo get_post_meta($post->ID, '_price', true); ?></span>
 			<?php the_content(); ?>
 		</div>
+		<hr class="rt-product-line">
 		
 	<?php endwhile;
 }
 else {
 	global $jade; 
 	$id = $jade->option('featured_product'); ?>
-	<h2 class=""><?php echo get_the_title($id); ?></h2>
-	<p>$<?php echo get_post_meta($id, '_price', true); ?></p>
-	<a class="" href="<?php echo get_post_meta($id, '_sample_profile', true); ?>" target="_blank">Sample Profile</a>
-	<p>
-		<?php 
-		$feat_post = get_post( $id );
-		echo $feat_post->post_content; ?>
-	</p>
+	<h2>Featured Product</h2>
+	<div class="rt-feat-product clearfix">
+		<h3><?php echo get_the_title($id); ?></h3>
+		<a class="rt-feat-product-preview" href="<?php echo get_post_meta($id, '_sample_profile', true); ?>" target="_blank">Sample Profile</a>
+		<p class="rt-feat-product-price">$<?php echo get_post_meta($id, '_price', true); ?></p>
+		<p class="rt-feat-product-description">
+			<?php 
+			$feat_post = get_post( $id );
+			echo $feat_post->post_content; ?>
+		</p>
+	</div>
 	
 <?php } 
 
